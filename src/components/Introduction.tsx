fc88373816b3c53bb5e5b3efc189390c50e63e83
Pieczasz <bartekp854@gmail.com>
2024-06-02 22:07:56 +0200

    Writed text for introduction and change styles of introduction container and recommendations container

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -5,24 +5,33 @@ const Introduction = () => {
     <div className="flex w-full flex-col border-b-[1.5px] border-t-[1.5px] border-primary/35 xl:flex-row">
       <div className="w-full items-center justify-center border-b-[1.5px] border-primary/35 px-10 py-20 xl:w-1/2 xl:border-r-[1.5px] xl:border-b-transparent">
         <div className="m-auto flex max-w-sm flex-col">
-          <h1 className=" mb-6 text-4xl font-bold">
+          <h2 className=" mb-6 text-4xl font-bold">
             Hi, I&apos;m Bartek Piekarz
-          </h1>
-          <p className="text-xl">
-            I&apos;m a full-stack developer from Poland. My history begin in may
-            2024.
-          </p>
+          </h2>
+          <div className="flex flex-col gap-y-6">
+            {/* //TODO: make it the acual post from blog and read more... */}
+            <p className="text-xl">
+              I&apos;m a full-stack developer from Poland. My history begin in
+              may 2024. At that time I started learning react by creating
+              website for my church parish (Yes I am catholic).
+            </p>
+            <p className="text-xl">
+              After that I started creating Vodkie using React Native expo.
+              Besides that at the same time I was creating blog that you are now
+              able to read. Read more...
+            </p>
+          </div>
         </div>
       </div>
       <div className=" w-full flex-col items-center justify-center border-primary/35 px-10 py-20 xl:w-1/2 xl:flex-row xl:border-r-[1.5px]">
-        <div className=" m-auto flex max-w-sm flex-col">
-          <h1 className="mb-6 text-4xl font-bold">
-            Hi, I&apos;m Bartek Piekarz
-          </h1>
-          <p className="text-xl">
-            I&apos;m a full-stack developer from Poland. My history begin in may
-            2024.
-          </p>
+        <div className="m-auto flex max-w-sm flex-col gap-y-12">
+          <h4 className="text-2xl font-bold">Browse my latest posts:</h4>
+          <div className="flex flex-col gap-y-12">
+            {/* //TODO: add latest posts */}
+            <p className="text-xl">Post 1</p>
+            <p className="text-xl">Post 2</p>
+            <p className="text-xl">Post 3</p>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Recommendations.tsx
+++ src/components/Recommendations.tsx
@@ -3,8 +3,10 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 const Recommendations = () => {
   return (
-    <div className="m-auto flex w-full flex-col items-center py-[8rem]">
-      <h2 className="text-3xl font-bold">Stay updated and sign in</h2>
+    <div className="m-auto flex w-full flex-col items-center px-10 py-[8rem]">
+      <h2 className="text-center text-3xl font-bold">
+        Stay updated and sign in
+      </h2>
       <h2 className="pt-1 text-3xl  font-bold">for my newsletter</h2>
       <div className="flex w-full max-w-sm items-center justify-center pt-6">
         <Input
