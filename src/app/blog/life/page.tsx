204792ee2dab841c831bcf92c205b980609aa06f
Pieczasz <bartekp854@gmail.com>
2024-06-04 22:04:49 +0200

    coding, productivity, trading, life

---
+++ src/app/blog/life/page.tsx
@@ -0,0 +1,11 @@
+import MaxWidthWrapper from "@/components/MaxWidthWrapper";
+
+export default async function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center text-white">
+      <MaxWidthWrapper>
+        <div></div>
+      </MaxWidthWrapper>
+    </main>
+  );
+}
