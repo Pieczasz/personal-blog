7d9e72c5162f728be6159791226c396273d6f14d
Pieczasz <bartekp854@gmail.com>
2024-06-12 11:17:41 +0200

    setup turso with drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,13 @@
 import { type Config } from "drizzle-kit";
 
-import { env } from "@/env";
-
 export default {
   schema: "./src/server/db/schema.ts",
-  dialect: "postgresql",
+  dialect: "sqlite",
+  driver: "turso",
   dbCredentials: {
-    url: env.DATABASE_URL,
+    url: process.env.DATABASE_URL!,
+    authToken: process.env.AUTH_TOKEN,
   },
   tablesFilter: ["personal-blog_*"],
+  out: "./drizzle",
 } satisfies Config;
