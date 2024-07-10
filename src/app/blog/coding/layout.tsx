18e20b3b446b33af8bf667db104c2e280f51b86e
Pieczasz <bartekp854@gmail.com>
2024-07-10 13:04:47 +0200

    improved layout for mobile devices

--- src/app/blog/coding/layout.tsx
+++ src/app/blog/coding/layout.tsx
@@ -3,6 +3,7 @@
 // Functions
 import Link from "next/link";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 // Icons
 import { FaSearch } from "react-icons/fa";
@@ -11,18 +12,112 @@ import { api } from "@/trpc/react";
 // Framer motion
 import { motion } from "framer-motion";
 
-function Coding({ children }: { children: React.ReactNode }) {
-  // getCodingPosts
-  const { data: posts, isLoading, error } = api.post.getCodingPosts.useQuery();
+function Life({ children }: { children: React.ReactNode }) {
+  const { data: posts, isLoading, error } = api.post.getLifePosts.useQuery();
 
   const router = useRouter();
 
+  const [showPosts, setShowPosts] = useState(false);
+
   return (
     <main className="flex min-h-screen flex-col items-end lg:flex-row">
-      <div className="flex w-full">{children}</div>
-      <div className="h-full min-h-screen border-l border-primary/35 lg:w-[39%] xl:w-[30%]">
+      {/* Top Bar on Mobile*/}
+      <div className="flex w-full flex-col items-center border-b border-primary/35 lg:hidden">
+        <hr className="w-full" />
+        <div className="my-3 flex w-full items-center justify-center px-4">
+          <input
+            type="text"
+            name="search"
+            id="search"
+            className="w-full p-1 focus:outline-none focus:ring-transparent"
+            placeholder="Search through posts"
+            onFocus={() => setShowPosts(true)}
+          />
+          <FaSearch
+            className="h-5 w-5 cursor-pointer hover:scale-[1.15]"
+            onFocus={() => setShowPosts(true)}
+          />
+        </div>
         <hr className="w-full border-primary/35" />
-        <div className="my-3 flex w-full max-w-md items-center justify-center gap-x-2 px-2">
+        <div className=" my-4 flex w-full items-center justify-center gap-x-4">
+          <div className="flex flex-col">
+            <motion.div
+              className="z-50 w-16 rounded-2xl bg-[#DAF7A6] text-center font-bold"
+              whileHover={{ y: -3 }}
+              whileTap={{ y: 7 }}
+            >
+              <Link href="/blog/life">
+                <h4 className="block p-2">Life</h4>
+              </Link>
+            </motion.div>
+            <div className="mt-[-2rem] h-10 w-16 rounded-2xl bg-[#DAF7A6]/35 px-4 py-2" />
+          </div>
+          <div className="flex flex-col">
+            <motion.div
+              className="z-50  w-32 rounded-2xl bg-[#FFC300] text-center font-bold"
+              whileHover={{ y: -3 }}
+              whileTap={{ y: 7 }}
+            >
+              <Link href="/blog/productivity">
+                <h4 className="block p-2">Productivity</h4>
+              </Link>
+            </motion.div>
+            <div className="mt-[-2rem] h-10 w-32 rounded-2xl bg-[#FFC300]/35 px-4 py-2" />
+          </div>
+          <div className="flex flex-col">
+            <motion.div
+              className="z-50  rounded-2xl bg-[#ADD8E6] text-center font-bold"
+              whileHover={{ y: -3 }}
+              whileTap={{ y: 7 }}
+            >
+              <Link href="/blog/coding">
+                <h4 className="block p-2">Coding</h4>
+              </Link>
+            </motion.div>
+            <div className="mt-[-2rem] h-10 w-20 rounded-2xl bg-[#ADD8E6]/35 px-4 py-2" />
+          </div>
+          <div className="flex flex-col">
+            <motion.div
+              className="z-50  rounded-2xl bg-[#F6846A] text-center font-bold"
+              whileHover={{ y: -3 }}
+              whileTap={{ y: 7 }}
+            >
+              <Link href="/blog/trading">
+                <h4 className="block p-2">Trading</h4>
+              </Link>
+            </motion.div>
+            <div className="mt-[-2rem] h-10 w-20 rounded-2xl bg-[#F6846A]/35 px-4 py-2" />
+          </div>
+        </div>
+      </div>
+      {/* Post List on Mobile */}
+      {showPosts && (
+        <div className="flex w-full flex-col lg:hidden">
+          {isLoading ? (
+            <div></div>
+          ) : error ? (
+            <p className="px-2">Error loading posts</p>
+          ) : (
+            posts!.map((post) => (
+              <motion.div
+                key={post.id}
+                whileHover={{ height: "4.5rem" }}
+                className="flex cursor-pointer items-center justify-start border-b border-y-primary/35 hover:bg-slate-100"
+                onClick={() => router.push(`/blog/life/${post.slug}`)}
+              >
+                <div className="flex h-full w-full items-center p-4 duration-200">
+                  <p>{post.title}</p>
+                </div>
+              </motion.div>
+            ))
+          )}
+        </div>
+      )}
+      <div className="flex w-full">{children}</div>
+      {/* Side bar */}
+      <div className="hidden h-full min-h-screen border-l border-primary/35 lg:block lg:w-[39%] xl:w-[30%]">
+        <hr className="w-full" />
+        <div className="my-3 flex w-full items-center justify-center px-4">
           <input
             type="text"
             name="search"
@@ -33,8 +128,9 @@ function Coding({ children }: { children: React.ReactNode }) {
           <FaSearch className="h-5 w-5 cursor-pointer hover:scale-[1.15]" />
         </div>
         <hr className="w-full border-primary/35" />
+
         <div className=" flex w-full flex-col border-b border-y-primary/35 py-5">
-          <div className="mb-4 flex w-full max-w-md items-center justify-center gap-x-4 px-2">
+          <div className="mb-4 flex w-full items-center justify-center gap-x-4 px-2">
             <div className="flex flex-col">
               <motion.div
                 className="z-50 min-w-16 rounded-2xl bg-[#DAF7A6] text-center font-bold"
@@ -93,6 +189,7 @@ function Coding({ children }: { children: React.ReactNode }) {
             </p>
           </div>
         </div>
+        {/* Post list */}
         <div className="flex w-full flex-col">
           {isLoading ? (
             <div></div>
@@ -104,7 +201,7 @@ function Coding({ children }: { children: React.ReactNode }) {
                 key={post.id}
                 whileHover={{ height: "4.5rem" }}
                 className="flex cursor-pointer items-center justify-start border-b border-y-primary/35 hover:bg-slate-100"
-                onClick={() => router.push(`/blog/coding/${post.slug}`)}
+                onClick={() => router.push(`/blog/life/${post.slug}`)}
               >
                 <div className="flex h-full w-full items-center p-4 duration-200">
                   <p>{post.title}</p>
@@ -118,4 +215,4 @@ function Coding({ children }: { children: React.ReactNode }) {
   );
 }
 
-export default Coding;
+export default Life;
