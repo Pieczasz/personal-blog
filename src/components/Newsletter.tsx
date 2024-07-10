8df79be406fd25b74854edd87bc5a5dc387e845c
Pieczasz <bartekp854@gmail.com>
2024-07-10 23:10:56 +0200

    changed backgournd of input to white

--- src/components/Newsletter.tsx
+++ src/components/Newsletter.tsx
@@ -64,10 +64,10 @@ const Newsletter = () => {
 
         if (widgetInput && widgetButton) {
           widgetForm.className =
-            "flex w-full max-w-sm items-center justify-center gap-x-4 py-6 mx-auto";
+            "flex w-full max-w-sm items-center justify-center gap-x-4 py-6 mx-auto bg-white";
 
           widgetInput.className =
-            "h-12 w-56 rounded-2xl border border-input px-3 py-2 text-sm placeholder:text-muted-foreground ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50";
+            "h-12 w-56 rounded-2xl border border-input px-3 py-2 text-sm placeholder:text-muted-foreground ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 bg-white";
 
           if (widgetSuccess) {
             widgetSuccess.className = "text-xl text-green-500";
