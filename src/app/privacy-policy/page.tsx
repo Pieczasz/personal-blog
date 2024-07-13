7a8cf441275f5c60b2d25f1d6fed558be5ac1d3a
Pieczasz <bartekp854@gmail.com>
2024-07-13 21:59:59 +0200

    PrivacyPolicy

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -9,23 +9,185 @@ function PrivacyPolicy() {
               By visiting BartekPiekarz.com, you are consenting to our privacy
               policy.
             </p>
-            <h4>
-              If you ask me how I do it, I will tell you that I don&apos;t know.
-              I feel that I was just born to do what I am doing. I created this
-              blog to help other teens be productive and motivated to learn and
-              do what they feel they should do.
-            </h4>
+            <h4>Overview</h4>
+            {/* //TODO: Bartek Piekarz Holdings LLC - name of company */}
             <p className="text-xl">
-              I hope you enjoy it and feel the same as I do. I am not trying to
-              fake it and be like those people on TikTok and YouTube who tell
-              you that making money is easy, life is easy, and everything will
-              always go as you would like it to go.
+              BartekPiekarz.com is committed to protecting your privacy online.
+              This Privacy Policy describes the information we collect through
+              this website (the “Site”), and how we collect and use such
+              information.
+              <br />
+              The terms “we,” “us,” and “our” refers to Bartek Piekarz Holdings
+              LLC. The terms “user,” “you,” and “your” refer to site visitors,
+              customers, and any other users of the site.
+              <br />
+              Use of BartekPiekarz.com, including all materials presented herein
+              and all online services provided by Bartek Piekarz Holdings LLC is
+              subject to the following Privacy Policy. This Privacy Policy
+              applies to all site visitors, customers, and all other users of
+              the site. By using the Site or Service, you agree to this Privacy
+              Policy, without modification, and acknowledge reading it.
             </p>
+            <h4>Cookies</h4>
+            {/* //TODO: Bartek Piekarz Holdings LLC - name of company */}
             <p className="text-xl">
-              I am here to show my perspective on life, work, and how I want to
-              become successful.
+              A cookie is a file containing an identifier (a string of letters
+              and numbers) that is sent by a web server to a web browser and is
+              stored by the browser. The identifier is then sent back to the
+              server each time the browser requests a page from the server.
+              Cookies may be either “persistent” cookies or “session” cookies: a
+              persistent cookie will be stored by a web browser and will remain
+              valid until its set expiry date, unless deleted by the user before
+              the expiry date; a session cookie, on the other hand, will expire
+              at the end of the user session, when the web browser is closed.
+              Cookies do not typically contain any information that personally
+              identifies a user, but personal information that we store about
+              you may be linked to the information stored in and obtained from
+              cookies.
+              <br />
+              We use cookies for the following purposes:
+              <br />
+              (a) (authentication – we use cookies to identify you when you
+              visit our website and as you navigate our website); (c)
+              (personalization – we use cookies (to store information about your
+              preferences and to personalize the website for you); (e)
+              (advertising – we use cookies (to help us to display
+              advertisements that will be relevant to you); and (f) (analysis –
+              we use cookies (to help us to analyze the use and performance of
+              our website and services);
+              <br />
+              We use Google Analytics to analyze the use of our website. Google
+              Analytics gathers information about website use by means of
+              cookies. The information gathered relating to our website is used
+              to create reports about the use of our website. Google’s privacy
+              policy is available at: https://www.google.com/policies/privacy/
+              <br />
+              Most browsers allow you to refuse to accept cookies and to delete
+              cookies. The methods for doing so vary from browser to browser,
+              and from version to version. You can however obtain up-to-date
+              information about blocking and deleting cookies via these links:
+              <br />
+              (a) https://support.google.com/chrome/answer/95647?hl=en (Chrome);
+              (b)
+              https://support.mozilla.org/en-US/kb/enable-and-disable-cookies-website-preferences(Firefox);
+              (c) http://www.opera.com/help/tutorials/security/cookies/ (Opera);
+              (d)
+              https://support.microsoft.com/en-gb/help/17442/windows-internet-explorer-delete-manage-cookies
+              (Internet Explorer); (e) https://support.apple.com/kb/PH21411
+              (Safari); and (f)
+              https://privacy.microsoft.com/en-us/windows-10-microsoft-edge-and-privacy
+              (Edge). Please note that blocking cookies may have a negative
+              impact on the functions of many websites, including our Site. Some
+              features of the Site may cease to be available to you.
+            </p>
+            <h4>Advertising</h4>
+            {/* //TODO: Bartek Piekarz Holdings LLC - name of company */}
+            <p className="text-xl">
+              By using this Site, you consent to the use of such cookies and the
+              sharing of data captured by such cookies with Google, and our
+              other third party partners.You can view, delete or add interest
+              categories associated with your browser by visiting:
+              https://adssettings.google.com. You can also opt out of the
+              network cookie using those settings or using the Network
+              Advertising Initiative’s multi-cookie opt-out mechanism at:
+              http://optout.networkadvertising.org. However, these opt-out
+              mechanisms themselves use cookies, and if you clear the cookies
+              from your browser your opt-out will not be maintained.
+              <br />
+              For European Economic Area Residents
+              <br />
+              If you reside in a country in the European Economic Area (EEA),
+              then under the GDPR and applicable data protection laws you have
+              the rights, among other things, to access your personal data, have
+              us erase it, and/or restrict its further processing. If you wish
+              to access or delete your personal data (if any) maintained by us
+              or AdThrive related to advertising on the Site, you can contact
+              AdThrive at info@adthrive.com. If you wish to see a list of the
+              advertising partners we work with or change which of those
+              partners track your behavior using cookies, click the ad
+              preferences icon. (Available in the EU.)
+            </p>
+            <h4>Affiliated sites, linked sites and advertisements</h4>
+            <p className="text-xl">
+              BartekPiekarz.com expects its partners, advertisers and affiliates
+              to respect the privacy of our users. Be aware, however, that third
+              parties, including our partners, advertisers, affiliates and other
+              content providers accessible through our site, may have their own
+              privacy and data collection policies and practices. For example,
+              during your visit to our site you may link to, or view as part of
+              a frame onBartekPiekarz.com pages, certain content that is
+              actually created or hosted by a third party. Also,
+              throughBartekPiekarz.com you may be introduced to, or be able to
+              access, information, Web sites, features, contests or sweepstakes
+              offered by other parties. BartekPiekarz.com is not responsible for
+              the actions or policies of such third parties. You should check
+              the applicable privacy policies of those third parties when
+              providing information on a feature or page operated by a third
+              party.
+              <br />
+              While on our site, our advertisers, promotional partners or other
+              third parties may use cookies or other technology to attempt to
+              identify some of your preferences or retrieve information about
+              you. For example, some of our advertising is served by third
+              parties and may include cookies that enable the advertiser to
+              determine whether you have seen a particular advertisement before.
+              Other features available on our site may offer services operated
+              by third parties and may use cookies or other technology to gather
+              information.BartekPiekarz.com does not control the use of this
+              technology by third parties or the resulting information, and are
+              not responsible for any actions or policies of such third parties.
+              <br />
+              You should also be aware that if you voluntarily disclose
+              Personally Identifiable Information on message boards or in chat
+              areas, that information can be viewed publicly and can be
+              collected and used by third parties without our knowledge and may
+              result in unsolicited messages from other individuals or third
+              parties. Such activities are beyond the control of
+              BartekPiekarz.com and this policy.
+            </p>
+            <h4>Additional Rights OF EEA (European Economic Area) Residents</h4>
+            <p className="text-xl">
+              If you are a resident of a country in the EEA, you have the
+              rights, among others, to:
+              <br />
+              (I) access your personal data
+              <br />
+              (II) ensure the accuracy of your personal data
+              <br />
+              (III) the right to have us delete your personal data
+              <br />
+              (IV) the right to restrict further processing of your personal
+              data, and
+              <br />
+              (V) the right to complain to a supervisory authority in your
+              country of residence in the event that data is misused
+              <br />
+              If you believe that our processing of your personal information
+              infringes data protection laws, you have a legal right to lodge a
+              complaint with a supervisory authority responsible for data
+              protection. You may do so in the EU member state of your
+              residence, your place of work or the place of the alleged
+              infringement. Please contact with us via email at
+              <br />
+              {/* //TODO: add here email */}
+            </p>
+            <h4>Changes to this Policy</h4>
+            <p className="text-xl">
+              BartekPiekarz.com reserves the right to change this policy at any
+              time. Please check this page periodically for changes. Your
+              continued use of our site following the posting of changes to
+              these terms will mean you accept those changes. Information
+              collected prior to the time any change is posted will be used
+              according to the rules and laws that applied at the time the
+              information was collected.
             </p>
           </div>
+          <h4>Contact</h4>
+          <p className="text-xl">
+            If you have any questions about this Privacy Policy, or the
+            practices of this Site, please contact us at:
+          </p>
+          {/* //TODO: add here email */}
         </div>
       </div>
     </main>
